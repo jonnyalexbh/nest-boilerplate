f400b5114776b3bca4d32125779f5bbae13cb30a
jonnyalexbh <jonnyalex.bh@gmail.com>
2021-12-28 11:10:38 -0500

    Methods http

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,19 +1,41 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+  // HttpStatus,
+  // HttpCode,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  getUsers() {
+  get() {
     return { message: 'Get all users' };
   }
 
   @Get(':id')
-  getUsersAll(@Param() userId: number) {
+  // @HttpCode(HttpStatus.FORBIDDEN)
+  getById(@Param('id') userId: number) {
     return { message: `Get user with id ${userId}` };
   }
 
   @Post()
-  storeUser() {
-    return 'ok';
+  create(@Body() payload: any) {
+    console.log('**** Payload ****', payload);
+    return { message: 'ok' };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return { id, payload };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') userId: number) {
+    return { message: `Delete user with id ${userId}` };
   }
 }
