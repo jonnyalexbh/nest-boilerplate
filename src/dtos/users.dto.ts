7ed3b592d6d166e00ecb6b05a47e5d32bf0a08ab
jonnyalexbh <jonnyalex.bh@gmail.com>
2021-12-30 11:21:00 -0500

    Validating parameters with class-validator and mapped-types

--- src/dtos/users.dto.ts
+++ src/dtos/users.dto.ts
@@ -1,11 +1,22 @@
+import {
+  IsString,
+  IsNumber,
+  IsEmail,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly age: number;
+  @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
 }
 
-export class UpdateUserDto {
-  readonly name?: string;
-  readonly age?: number;
-  readonly email?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
